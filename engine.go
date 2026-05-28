193f10279105b764f1e961b91a6c0afc8b737b76
gddisney <gregory.disney@owasp.org>
2026-05-28 16:48:47 -0400

    Update engine.go

--- engine.go
+++ engine.go
@@ -1,8 +1,8 @@
 package orchid_sync
 
 import (
-	"context"
 	"encoding/json"
+	"fmt"
 	"sort"
 	"strings"
 	"sync"
@@ -10,7 +10,6 @@ import (
 	"github.com/gddisney/logger"
 	"github.com/gddisney/secure_network"
 	"github.com/gddisney/ultimate_db"
-	"github.com/gddisney/webauthnext"
 )
 
 type Engine struct {
@@ -25,24 +24,23 @@ type Engine struct {
 	TotalDocLen uint64
 }
 
-func NewEngine(db *ultimate_db.DB, node *secure_network.SecureNode, sharding *ConsistentHashRing, sysLog *logger.LogDispatcher) (*Engine, error) {
-	eng := &Engine{db: db, netNode: node, sharding: sharding, analyzer: NewAnalyzer(), logger: sysLog}
-	return eng, nil
-}
-
 func (e *Engine) Index(docID string, text string) error {
-    // ... (Your implementation)
+	// ... (Your existing Index implementation)
     return nil
 }
 
 func (e *Engine) DetermineRelevantShards(query string) []uint64 {
+	if e.sharding == nil { return []uint64{0} }
 	terms := tokenize(query)
 	shardSet := make(map[uint64]struct{})
 	for _, term := range terms {
 		shardID := e.computeShardID(term)
 		shardSet[shardID] = struct{}{}
 	}
-	return nil // ... sort and return
+	results := make([]uint64, 0, len(shardSet))
+	for id := range shardSet { results = append(results, id) }
+	sort.Slice(results, func(i, j int) bool { return results[i] < results[j] })
+	return results
 }
 
 func (e *Engine) computeShardID(term string) uint64 {
@@ -54,9 +52,8 @@ func tokenize(query string) []string {
 	fields := strings.Fields(query)
 	results := make([]string, 0)
 	for _, f := range fields {
-		if f = strings.TrimSpace(f); len(f) > 0 {
-			results = append(results, f)
-		}
+		f = strings.TrimSpace(f)
+		if len(f) > 0 { results = append(results, f) }
 	}
 	return results
 }
